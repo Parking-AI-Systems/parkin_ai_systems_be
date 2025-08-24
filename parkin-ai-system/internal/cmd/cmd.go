f1ba7453ee24444c2aa0e21809b878395e9f7585
huypqse <huypqse@gmail.com>
2025-08-24 23:34:39 +0700

    feat: add midddlewaare for sign up

--- parkin-ai-system/internal/cmd/cmd.go
+++ parkin-ai-system/internal/cmd/cmd.go
@@ -2,10 +2,13 @@ package cmd
 
 import (
 	"context"
+	"parkin-ai-system/internal/config"
+	"parkin-ai-system/internal/controller/user"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
+	"github.com/gogf/gf/v2/os/glog"
 )
 
 var (
@@ -14,12 +17,20 @@ var (
 		Usage: "main",
 		Brief: "start http server",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
+			g.Log().SetHandlers(glog.HandlerJson)
+			glog.SetHandlers(glog.HandlerJson)
+
+			config.InitConfig(ctx)
+
+			userCtrl := user.NewUser()
+
 			s := g.Server()
-			s.Group("/", func(group *ghttp.RouterGroup) {
+
+			s.Logger().SetHandlers(glog.HandlerJson)
+
+			s.Group("/backend/parkin/v1", func(group *ghttp.RouterGroup) {
 				group.Middleware(ghttp.MiddlewareHandlerResponse)
-				group.Bind(
-				// hello.NewV1(),
-				)
+				group.POST("/user/register", userCtrl.Register)
 			})
 			s.Run()
 			return nil
--- parkin-ai-system/internal/controller/user/user_user_register.go
+++ parkin-ai-system/internal/controller/user/user_user_register.go
@@ -7,8 +7,13 @@ import (
 	"github.com/gogf/gf/v2/errors/gerror"
 
 	"parkin-ai-system/api/user/user"
+	"parkin-ai-system/internal/service"
 )
 
 func (c *ControllerUser) Register(ctx context.Context, req *user.RegisterReq) (res *user.RegisterRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	res, err = service.User().SignUp(ctx, req)
+	if err != nil {
+		return nil, gerror.NewCode(gcode.CodeInternalError)
+	}
+	return
 }
