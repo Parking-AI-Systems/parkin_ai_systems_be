3cbb73d394da193626130654784568b93821d571
huypqse <huypqse@gmail.com>
2025-08-26 00:02:57 +0700

    fix: fix template

--- parkin-ai-system/internal/controller/user/user_user_profile.go
+++ parkin-ai-system/internal/controller/user/user_user_profile.go
@@ -7,5 +7,9 @@ import (
 )
 
 func (c *ControllerUser) UserProfile(ctx context.Context, req *user.UserProfileReq) (res *user.UserProfileRes, err error) {
-	return service.User().UserProfile(ctx, req)
+	res, err = service.User().UserProfile(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return
 }
